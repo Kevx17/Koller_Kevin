b6848e922bfb05257a8c344531cb51259b16e2cc
Kevx17 <kevinkokobello@gmail.com>
2025-02-27 19:49:36 +0100

    created test projects section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,13 +1,30 @@
-import styles from './ProjectsStyles.modules.css';
+import styles from './ProjectsStyles.module.css';
+import ProjectCard from '../common/ProjectCard';
+import img from '../../assets/images.png'
 
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
-            <a href="">
-                <img className='hover' src="" alt="" />
-            </a>
+            <ProjectCard
+              src={img}
+              link='google.com'
+              h3='Project1'
+              p='short description'
+            />
+            <ProjectCard
+              src={img}
+              link='google.com'
+              h3='Project2'
+              p='short description'
+            />
+            <ProjectCard
+              src={img}
+              link='google.com'
+              h3='Project3'
+              p='short description'
+            />
         </div>
     </section>
   )
